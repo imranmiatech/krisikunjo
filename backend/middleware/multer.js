410363b849e9b7ad4fdccf5e4d94d12cac0b6778
Imran <128302897+imranmiatech@users.noreply.github.com>
2025-12-16 20:09:16 +0600

    Update multer.js

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -14,21 +14,21 @@
 
 import multer from "multer";
 
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, "uploads/");   // folder name
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, Date.now() + "-" + file.originalname); // Correct
-//     }
-// });
-const storage = new CloudinaryStorage({
-  cloudinary,
-  params: {
-    folder: "products",
-    allowed_formats: ["jpg", "png", "jpeg"],
-  },
-})
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "uploads/");   // folder name
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname); // Correct
+    }
+});
+// const storage = new CloudinaryStorage({
+//   cloudinary,
+//   params: {
+//     folder: "products",
+//     allowed_formats: ["jpg", "png", "jpeg"],
+//   },
+// })
 const upload = multer({ storage });
 
 export default upload;
