a1f123bf5bd683a5a50491a19d03fcd3461966e9
Mdimran19 <imran32472@gmail.com>
2025-12-28 20:27:05 +0600

    up

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,25 +1,25 @@
 
- import multer from "multer";
+//  import multer from "multer";
 
- const storage = multer.diskStorage({
-     destination: function (req, file, cb) {
-         cb(null, "uploads/");   // folder name
-     },
-     filename: function (req, file, cb) {
-         cb(null, Date.now() + "-" + file.originalname); // Correct
-     }
- });
+//  const storage = multer.diskStorage({
+//      destination: function (req, file, cb) {
+//          cb(null, "uploads/");   // folder name
+//      },
+//      filename: function (req, file, cb) {
+//          cb(null, Date.now() + "-" + file.originalname); // Correct
+//      }
+//  });
 
 
- export default upload;
+//  export default upload;
 
-// import multer from "multer";
+import multer from "multer";
 
-// const storage = multer.memoryStorage();
+const storage = multer.memoryStorage();
 
-// const upload = multer({
-//   storage,
-//   limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
-// });
+const upload = multer({
+  storage,
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB
+});
 
-// export default upload;
+export default upload;
