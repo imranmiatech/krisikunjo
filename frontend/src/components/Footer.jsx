7e9754c2786db082723e9ed34ab86fdb0676db3a
Imran <128302897+imranmiatech@users.noreply.github.com>
2026-01-31 17:52:51 +0600

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 const Footer = () => {
   return (
     <div>
-      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 md:mt-40 text-sm">
         <div>
            <Link to="/">
                   {/* <img src={assets.OO1} width={78} height={40} alt="" /> */}
